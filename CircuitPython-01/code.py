d064ac4f97ac513e736394eb89dbcc592a720eb2
Evgeny Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-10-24 19:07:50 -0400

    done with second guide

---
+++ CircuitPython-01/code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Evgeny Vovk
+# Created on: 2022 Oct
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+def game_scene():
+    
+    # this function is the main game game_scene
+
+    print("\n\n\n")
+    print("Hello, World!")
+
+    while True:
+        pass # just a placeholder for now
+    
+if __name__ == "__main__":
+    game_scene()
